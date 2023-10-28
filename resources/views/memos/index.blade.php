64b5014a198a58b9d6946ee2253fddb4f952fa18
lastsamurai24 <lastsamurai24@icloud.com>
2023-10-28 17:54:11 +0900

    route

---
+++ resources/views/memos/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index</title>
+</head>
+
+<body>
+    <h2>
+        <h1>出品メモ一覧</h1>
+        <ul>
+            <!-- リンク先をidで取得し名前で出力 -->
+            @foreach ($memos as $memo)
+            <li><a href="{{ route('memos.show', $memo) }}">{{ $memo->title }}</a></li>
+            @endforeach
+        
+        </ul>
+    </h2>
+</body>
+
+</html>
