6d3f1d212bd7e4195ca0be59469dddbddf0ffc6c
lastsamurai24 <lastsamurai24@icloud.com>
2023-10-29 13:33:00 +0900

    render

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use App\Http\Controllers\MemoController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\MemoController::class, 'index']);
 Route::resource ('memos',MemoController::class);
 // リソースを使用しない場合
 // Route::get('/memos', [MemoController::class, 'index']);
